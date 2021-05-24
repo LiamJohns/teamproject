39e53938ceaaeef9b2bab2c3635200884893ae9e
Liam Johns <270121836@yoobeestudent.ac.nz>
2021-05-24 14:22:13 +1200

    added header files

---
+++ teamc++/teamc++/teamc++.cpp
@@ -0,0 +1,25 @@
+// teamc++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //liam
+    //------------------
+
+    //------------------
+
+
+
+    //izzy
+    //-------------------
+
+    //-------------------
+
+
+    std::cout << "Hello World!\n";
+}
+
